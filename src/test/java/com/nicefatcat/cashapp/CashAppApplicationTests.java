71a8caad5f5622eebabb9d23568138e981a70535
“subartowicz” <subartowicz@gmail.com>
2025-05-31 09:57:43 +0200

    Initial commit

---
+++ src/test/java/com/nicefatcat/cashapp/CashAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicefatcat.cashapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
